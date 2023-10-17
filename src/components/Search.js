ec998ae3cb99c1e9177a52d8dff5fe1e308d0501
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-10-17 09:30:37 +0100

    Axios cannot be imported.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import "../styles/search.css";
+import getImages from "../requests/getImages";
 
 const Search = () => {
   const [value, setValue] = useState();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
-    <>
-      <form className="search-form">
+    <div className="search">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-input"
           type="text"
           onChange={(e) => setValue(e.target.value)}
         />
         <button className="search-btn" type="submit">
-          Search
-        </button>      
+          Go!
+        </button>
       </form>
-    </>
+    </div>
   );
 };
 
